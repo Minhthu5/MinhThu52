30e558df45d1ae2dbaaff4f47cbcab644f4c7e68
Minhthu5 <immthu5227@gmail.com>
2024-08-23 20:46:16 +0700

    Add files via upload

---
+++ DHQG.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 23 19:57:59 2024
+
+@author: PhamNguyenThiMinhThu
+"""
+
+input_string = "Đại học Quốc gia, Khu phố 6, P. Linh Trung, Q. Thủ Đức, Tp. HCM"
+parts = [part.strip() for part in input_string.split(',')]
+sub_strings = [
+    parts[0],
+    parts[1],
+    parts[2].split('P. ')[1],
+    parts[3].split('Q. ')[1],
+    parts[4].split('Tp. ')[1] ]
+for sub_string in sub_strings:
+    print (sub_string)
